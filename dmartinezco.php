8a2f0297bfcc38303539dc99f27ae8837ab5c197
Dave Martinez <martind2@oregonstate.edu>
2018-03-04 22:33:40 -0800

    Initial commit

---
+++ dmartinezco.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: dmartinez.co
+Plugin URI: https://dmartinez.co
+Description: Provides several pieces of functionality that enable a React-based wordpress theme
+Version: 0.1.0
+Author: Dave Martinez
+Author URI: http://dmartinez.co
+Text Domain: dmartinezco
+Domain Path: /dmartinezco
+*/
+
+/**
+ * Add menus to REST API
+ */
+
+function get_menu ($req) {
+  $menu = wp_get_nav_menu_object($req['id']);
+  $menu->items = wp_get_nav_menu_items($req['id']);
+  return $menu;
+}
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'dmco', '/menu/(?P<id>\d+)', array(
+    'methods' => 'GET',
+    'callback' => 'get_menu',
+  ) );
+} );
+
+ ?>
