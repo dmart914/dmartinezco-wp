c37b25553aff90c4f293ceeb1ae228dad06d0d44
Dave Martinez <martind2@oregonstate.edu>
2018-03-22 21:46:50 -0700

    Adding menu locations

--- dmartinezco.php
+++ dmartinezco.php
@@ -25,12 +25,38 @@ function get_menu ($req) {
   return $menu;
 }
 
+function get_menu_locations($req) {
+  $menu_locations = get_nav_menu_locations();
+  $menu_obj = get_term( $menu_locations[ $req['id'] ], 'nav_menu');
+  $menu_obj->items = wp_get_nav_menu_items($menu_obj->term_id);
+  return $menu_obj;
+}
+
+function get_front_page($req) {
+  $page_id = get_option( 'page_on_front' );
+  $page_obj = get_post($page_id);
+  return $page_obj;
+}
+
 add_action( 'rest_api_init', function () {
+  register_rest_route('dmco', '/menu/location/(?P<id>\w+)', array(
+    'methods' => 'GET',
+    'callback' => 'get_menu_locations',
+  ) );
+
   register_rest_route( 'dmco', '/menu/(?P<id>\d+)', array(
     'methods' => 'GET',
     'callback' => 'get_menu',
   ) );
+
+  register_rest_route('dmco', '/front_page', array(
+    'methods' => 'GET',
+    'callback' => 'get_front_page',
+  ));
 } );
 
+register_nav_menus( array(
+	'dmco_primary_menu' => 'React Primary Menu',
+) );
 
 ?>
